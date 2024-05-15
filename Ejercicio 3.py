f79080a0c034d78771295e32c065fb536aaf77f9
KZLenin <131980094+KZLenin@users.noreply.github.com>
2024-05-15 12:04:28 -0500

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,20 @@
+print("Instagram")
+user = input("Phone number: ")
+passwrd = input("Password: ")
+
+if user=="Lenin" and passwrd=="123":
+    print(f"Welcome{user}")
+    print("Loading your post")
+else: 
+    print("Sorry, your password was incorrect. Please double-check your password.")
+
+print("Sign up to see photos and videos from your friends.")
+input("Log in with Facebook")
+fb = input("Yes or No")
+if fb=="Yes":
+    print("Welcome")
+else: 
+    mobile = input("Mobile number or email")
+    name = input("Full name")
+    username = input("Username")
+    password = input("Password")
